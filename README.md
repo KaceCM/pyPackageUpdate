d82583209412471edaeebdb98f3d50a291be6124
Morgan Le Treust <morgan.le-treust@etu.univ-nantes.fr>
2023-02-09 12:51:14 +0100

    updating README.md

---
+++ README.md
@@ -0,0 +1,68 @@
+<p align="center">
+  <a href="https://www.linkedin.com/in/morganlt/">
+    <img src="https://media.licdn.com/dms/image/C4E0BAQHORDYRLBlyGg/company-logo_200_200/0/1652886251527?e=1683763200&v=beta&t=nX7UH0FoHg7LbTXZQJ4OsR2Hd0x9F6rIj0uE5C4FgyE" alt="Logo" width=72 height=72>
+  </a>
+
+  <h3 align="center">KaceCorp</h3>
+
+  <p align="center">
+    If you need to contact me, please click on the logo.
+    <br>
+    <a href="https://github.com/KaceCM/pyPackageUpdate/issues/new?template=bug.md">Report bug</a>
+    ·
+    <a href="https://github.com/KaceCM/pyPackageUpdate/issues/new?template=feature.md&labels=feature">Request feature</a>
+  </p>
+</p>
+
+
+### Table of contents
+
+- [Description](#Description)
+- [Requirements](#Requirements)
+- [Execution](#Execution)
+- [TO-DO list](#TO-DO-list)
+- [Thanks](#thanks)
+
+
+# Description
+
+Simple script to automatically update all python packages installed with PIP on a system.
+
+<br>
+
+
+# Requirements
+
+- You need to have <b>PYTHON</b> installed on your system. (<i>You can download it from [here](https://www.python.org/downloads/)</i>).
+- You need to have <b>pip</b> installed on your system. (<i>You can download it from [here](https://pip.pypa.io/en/stable/installing/)</i>).
+- You need to have <b>PIP INSTALLED IN PATH</b>. You can check if it is installed in PATH by running the following command in a terminal:
+```bash
+
+>>> pip --version
+
+```
+
+# Execution
+
+Simply execute ```pyPackageUpdate.py``` with python. You can use CMD command :
+
+```bash
+>>> python.exe pyPackageUpdate.py
+```
+
+# TO-DO list
+
+- [x] Create a README.md
+- [x] Post-update script
+- [ ] Adapt to Linux
+- [ ] Adapt to MacOS
+- [ ] Executable file 
+## Thanks
+
+Feel free to use this script and to contribute to it.
+
+- [Morgan L.](https://www.linkedin.com/in/morganlt/)
+- [KaceCorp](https://www.linkedin.com/company/kacecorp/)
+- [KaceCM](https://www.twitter.com/kacecm/)
+
+Enjoy :metal:
--- pyPackageUpdate.py
+++ pyPackageUpdate.py
@@ -1,61 +1,71 @@
 import os
-import subprocess
-from tqdm import tqdm
+
 current_path = os.getcwd()
 
 
-
 def save_outdated():
-    print('~ GETTING OUTDATED PACKAGES ... ~')
-    os.system(f'pip list --outdated > DATA/piplist_outdated.txt')
-    print('~ DONE ~\n')
+    print("~ GETTING OUTDATED PACKAGES ... ~")
+    if not os.path.exists(f"{current_path}/DATA"):
+        os.mkdir(f"{current_path}/DATA")
+    os.system(f"pip list --outdated > DATA/piplist_outdated.txt")
+    print("~ DONE ~\n")
     return True
 
-def create_update_list(method='pre'):
-    packages_name = []
-    path = f'{current_path}/DATA/piplist_outdated.txt' if method=='pre' else f'{current_path}/DATA/piplist_outdated_post.txt'
 
-    with open(f'{path}', 'r', encoding='utf-8') as f:
+def create_update_list(method="pre"):
+    packages_name = []
+    path = (
+        f"{current_path}/DATA/piplist_outdated.txt"
+        if method == "pre"
+        else f"{current_path}/DATA/piplist_outdated_post.txt"
+    )
+
+    with open(f"{path}", "r", encoding="utf-8") as f:
         content = f.read().strip().splitlines()
 
     for ind, vals in enumerate(content):
         if ind > 1:
-            packages_name.append(vals.split(' ')[0])
-            
-    return packages_name
+            packages_name.append(vals.split(" ")[0])
 
+    return packages_name
 
 
 def update_list(packages_list):
     if len(packages_list) == 0:
-        print('~ NO PACKAGES TO UPDATE ~')
+        print("~ NO PACKAGES TO UPDATE ~")
         return True
 
-    print('~ UPDATING PACKAGES ... ~')
-    for package in tqdm(packages_list):
-        os.system(f'pip install --upgrade {package}')
-    print('~ EVERYTHING ENDED CORRECTLY ~\n')
+    print("~ UPDATING PACKAGES ... ~")
+    for package in packages_list:
+        os.system(f"pip install --upgrade {package}")
+    print("~ EVERYTHING ENDED CORRECTLY. LAST CHECK... ~\n")
 
     return True
 
+
 def check_still_outdated():
-    os.system(f'pip list --outdated > DATA/piplist_outdated_post.txt')
-    packages_list_post = create_update_list(method='post')
+    os.system(f"pip list --outdated > DATA/piplist_outdated_post.txt")
+    #os.remove(f"{current_path}/DATA/piplist_outdated.txt")
+    packages_list_post = create_update_list(method="post")
     if len(packages_list_post) == 0:
+        os.remove(f"{current_path}/DATA/piplist_outdated_post.txt")
+        print("~ EVERYTHING IS UP TO DATE ~\n")
         return True
-    still_outdated = ', '.join(packages_list_post)
-    print(f'~ STILL OUTDATED PACKAGES (probably dependency problems): {still_outdated} ~')
-    print(f' CHECK DATA/piplist_outdated_post.txt FOR MORE DETAILS ~\n')
-    os.remove(f'{current_path}/DATA/piplist_outdated.txt')
+    still_outdated = ",\n".join(packages_list_post)
+    print(
+        f"~ STILL OUTDATED PACKAGES (probably dependency problems) : ~ \n{still_outdated}"
+    )
+    print(f"~ CHECK DATA/piplist_outdated_post.txt FOR MORE DETAILS ~\n")
     return False
+
 
 def main_pip_update():
     save_outdated()
     packages_list = create_update_list()
     update_list(packages_list)
-    check_still_outdated()
-    return True
+    final_state = check_still_outdated()
+    return final_state
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main_pip_update()
-    
